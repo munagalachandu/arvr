22420a10e45151c318b3d741973b5e0c2d54d8bc
munagalachandu <munagalachanduu@gmail.com>
2026-02-05 15:34:30 +0530

    labs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-app.use(express.static(path.join(__dirname,'publiic')));
+app.use(express.static(path.join(__dirname,'public')));
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
 console.log(`server running on http://localhost:${PORT}`);
